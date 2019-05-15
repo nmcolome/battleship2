5dbbaabde2efeff8ded5c1cf32034503daeab515
NataliaColome <nmcolome@gmail.com>
2019-05-15 17:43:55 -0600

    Add tests for #valid_placement? and its conditions

--- test/board_test.rb
+++ test/board_test.rb
@@ -6,6 +6,8 @@ require './lib/cell'
 class BoardTest < Minitest::Test
   def setup
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
@@ -24,5 +26,27 @@ class BoardTest < Minitest::Test
     refute @board.valid_coordinate?("A5")
     refute @board.valid_coordinate?("E1")
     refute @board.valid_coordinate?("A22")
+  end
+
+  def test_valid_placement_matches_number_of_coordinates_with_ship_length
+    refute @board.valid_placement?(@cruiser, ["A1", "A2"])
+    refute @board.valid_placement?(@submarine, ["A2", "A3", "A4"]) 
+  end
+
+  def test_valid_placement_verifies_coordinates_are_consecutive
+    refute @board.valid_placement?(@cruiser, ["A1", "A2", "A4"])
+    refute @board.valid_placement?(@submarine, ["A1", "C1"])
+    refute @board.valid_placement?(@cruiser, ["A3", "A2", "A1"])
+    refute @board.valid_placement?(@submarine, ["C1", "B1"])
+  end
+
+  def test_valid_placement_verifies_coordinates_are_not_diagonal
+    refute @board.valid_placement?(@cruiser, ["A1", "B2", "C3"])
+    refute @board.valid_placement?(@submarine, ["C2", "D3"])
+  end
+
+  def test_valid_placement_verifies_all_three_conditions
+    assert @board.valid_placement?(@submarine, ["C2", "D3"])
+    assert @board.valid_placement?(@cruiser, ["B1", "C1", "D1"])
   end
 end
