6706fd4597f9742cb2fc81cf005ab7df07f44650
NataliaColome <nmcolome@gmail.com>
2019-05-24 19:54:18 -0600

    Add logic for player ship placement and render starting boards

--- lib/battleship_runner.rb
+++ lib/battleship_runner.rb
@@ -22,18 +22,37 @@ class BattleshipRunner
 
   def setup
     computer_setup
-    # puts "You now need to lay out your two ships.\nThe Cruiser is two units long and the Submarine is three units long."
-    # @player_board.render
-    # puts "To place your ships enter your coordinates as: A1 A2..."
-    # puts "Enter the squares for the Cruiser (2 spaces):"
-    # cells = gets.chomp
-    # if @computer_board.valid_placement?(ship, cells)
-    #   @computer_board.place(ship, cells)
-    # else
-    #   puts "Those are invalid coordinates. Please try again:"
-    # end
+    puts "You now need to lay out your two ships.\nThe Cruiser is two units long and the Submarine is three units long."
+    puts @player_board.render
+    puts "To place your ships enter your coordinates as: A1 A2..."
+    player_setup
+    puts "Ok! Let's play:"
+    puts "=============COMPUTER BOARD============="
+    puts @computer_board.render
+    puts "==============PLAYER BOARD=============="
+    puts @player_board.render(true)
   end
-  
+
+  def player_setup
+    ships = [@p_cruiser, @p_submarine]
+    ships.each do |ship|
+      puts "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
+      coordinates_prompt(ship)
+    end
+  end
+
+  def coordinates_prompt(ship)
+    input = gets.chomp
+    cells = input.split(" ")
+    if @player_board.valid_placement?(ship, cells)
+      @player_board.place(ship, cells)
+      puts @player_board.render(true)
+    else
+      puts "Those are invalid coordinates. Please try again:"
+      coordinates_prompt(ship)
+    end
+  end
+
   def computer_setup
     ships = [@c_cruiser, @c_submarine]
     ships.each { |ship| assign_coordinates(ship) }
