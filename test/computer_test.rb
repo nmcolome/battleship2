dfc27e510a1883fdc13b162c0d9c5e934c718ba2
NataliaColome <nmcolome@gmail.com>
2019-05-27 14:56:18 -0600

    Add test_helper and SimpleCov. Correct computer_test

--- test/computer_test.rb
+++ test/computer_test.rb
@@ -1,10 +1,11 @@
-require 'minitest/autorun'
-require 'minitest/pride'
+require './test/test_helper.rb'
 require './lib/computer'
+require './lib/player'
 
 class ComputerTest < Minitest::Test
   def setup
     @computer = Computer.new
+    @player = Player.new
   end
 
   def test_the_coordinates_generator_for_the_computer_ship_placement
@@ -17,10 +18,10 @@ class ComputerTest < Minitest::Test
   end
 
   def test_it_shoots_only_new_cells
-    cells = @computer.board.cells
+    cells = @player.board.cells
     fired_cells = ["A1", "A2", "A3", "A4", "C1", "C3", "D2", "D3"].each { |key| cells[key].fire_upon }
     available_cells = ["B1", "B2", "B3", "B4", "C2", "C4", "D1", "D4"]
 
-    assert available_cells.include?(@computer.shoot)
+    assert available_cells.include?(@computer.shoot(@player.board))
   end
 end
