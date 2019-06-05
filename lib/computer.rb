33c9bcd3142815ef4e46dc339104c86e93911171
NataliaColome <nmcolome@gmail.com>
2019-06-05 13:06:00 -0600

    Add logic for computer shot based on previous hits

--- lib/computer.rb
+++ lib/computer.rb
@@ -51,15 +51,55 @@ class Computer < User
     board.cells.keys.find_all { |key| board.cells[key].fired_upon? == false }
   end
 
+  def hits(board)
+    @shots.select { |shot| board.cells[shot].render == 'H' }
+  end
+
+  def available_surrounding_cells(player_board)
+    last_hit = hits(player_board)[-1].split('')
+    row, col = last_hit[0], last_hit[1].to_i
+    surrounding_rows = surrounding_rows(player_board, row)
+    surrounding_cols = surrounding_cols(player_board, col)
+    horizontal = Array.new(surrounding_cols.length, row)
+    horiz_cells = horizontal.zip(surrounding_cols)
+    vertical = Array.new(surrounding_rows.length, col)
+    verti_cells = (surrounding_rows).zip(vertical)
+    pairs = horiz_cells | verti_cells
+    cells = pairs.map { |pair| pair.join('') }
+    cells.select { |shot| player_board.cells[shot].fired_upon? == false }
+  end
+
+  def surrounding_rows(board, row)
+    if board.rows[0] == row
+      board.rows[0..1]
+    elsif board.rows[-1] == row
+      board.rows[-2..-1]
+    else
+      row_i = board.rows.index(row)
+      board.rows[(row_i - 1)..(row_i + 1)]
+    end
+  end
+
+  def surrounding_cols(board, col)
+    if board.cols[0] == col
+      board.cols[0..1]
+    elsif board.cols[-1] == col
+      board.cols[-2..-1]
+    else
+      col_i = board.cols.index(col)
+      board.cols[(col_i - 1)..(col_i + 1)]
+    end
+  end
+
   def shoot(player_board)
-    binding.pry
     if @shots.empty?
       shot = player_board.cells.keys.sample
       player_board.cells[shot].fire_upon
       @shots << shot
-    elsif player_board.cells[@shots[-1]].render == "H" || player_board.cells[@shots[-2]].render == "H"
-      #
-      #do someting
+    elsif !hits(player_board).empty?
+      shot = available_surrounding_cells(player_board).sample
+      player_board.cells[shot].fire_upon
+      @shots << shot
     else
       shot = available_cells(player_board).sample
       player_board.cells[shot].fire_upon
